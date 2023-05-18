6d20ea79b2aa9edb2bbb2fa385c8f89916b9f3db
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-18 15:58:10 +0000

    Initial project - Spring initializr Extension

---
+++ src/test/java/com/alexquazar/springpractiverecipes/SpringpractiveRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexquazar.springpractiverecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpractiveRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
