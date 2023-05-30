c334d35cf2e336bb043c04d62620b142b56a6f65
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-30 12:55:29 +0000

    JPA Query Methods to find by description

--- src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/IndexController.java
+++ src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.alexquazar.SpringPracticeRecipes.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.alexquazar.SpringPracticeRecipes.model.Category;
+import com.alexquazar.SpringPracticeRecipes.model.UnitOfMeasure;
+import com.alexquazar.SpringPracticeRecipes.repositories.CategoryRepository;
+import com.alexquazar.SpringPracticeRecipes.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
-    @GetMapping({"", "/","index"})
-    public String getIndexPage(){
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @GetMapping({ "", "/", "index" })
+    public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM IDS is: " + unitOfMeasureOptional.get().getId());
+        
         return "index";
     }
 }
