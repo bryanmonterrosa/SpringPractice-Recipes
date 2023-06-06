997fbe9db17b0d0f981a862bc99c24450f3fb5f5
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-06-06 17:23:18 +0000

    JUnit and Mockito Tests

--- src/test/java/com/alexquazar/SpringPracticeRecipes/SpringPracticeRecipesApplicationTests.java
+++ src/test/java/com/alexquazar/SpringPracticeRecipes/SpringPracticeRecipesApplicationTests.java
@@ -1,13 +1,14 @@
-package com.alexquazar.springpractiverecipes;
+package com.alexquazar.SpringPracticeRecipes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringpractiveRecipesApplicationTests {
+class SpringPracticeRecipesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
---
+++ src/test/java/com/alexquazar/SpringPracticeRecipes/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.alexquazar.SpringPracticeRecipes.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.alexquazar.SpringPracticeRecipes.model.Recipe;
+import com.alexquazar.SpringPracticeRecipes.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void testGetRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
