3dd8ab5242862b12a1fe4afc0e9bd5c7d1430e9e
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-06-06 19:08:57 +0000

    Mockito Argument Capture

--- src/test/java/com/alexquazar/SpringPracticeRecipes/controllers/IndexControllerTest.java
+++ src/test/java/com/alexquazar/SpringPracticeRecipes/controllers/IndexControllerTest.java
@@ -1,17 +1,22 @@
 package com.alexquazar.SpringPracticeRecipes.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.alexquazar.SpringPracticeRecipes.model.Recipe;
 import com.alexquazar.SpringPracticeRecipes.services.RecipeService;
 
 public class IndexControllerTest {
@@ -31,11 +36,26 @@ public class IndexControllerTest {
 
     @Test
     void testGetIndexPage() {
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
 
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
         String viewName = controller.getIndexPage(model);
 
+        // then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
