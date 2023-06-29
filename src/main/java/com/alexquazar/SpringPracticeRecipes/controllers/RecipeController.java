5221232e4cf96397696d90bf42e5facbc9da4ee0
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-06-29 19:57:55 +0000

    Implemented Spring MVC exception handler

--- src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/RecipeController.java
+++ src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/RecipeController.java
@@ -1,14 +1,20 @@
 package com.alexquazar.SpringPracticeRecipes.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alexquazar.SpringPracticeRecipes.commands.RecipeCommand;
+import com.alexquazar.SpringPracticeRecipes.exceptions.NotFoundException;
 import com.alexquazar.SpringPracticeRecipes.services.RecipeService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -55,5 +61,14 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 }
